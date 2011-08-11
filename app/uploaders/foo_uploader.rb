fd8c49a4aa74a36196c046df54e3c00b95b7c353
Trevor Turk <trevorturk@gmail.com>
2011-08-10 22:42:32 -0500

    tested

--- app/uploaders/foo_uploader.rb
+++ app/uploaders/foo_uploader.rb
@@ -1,15 +1,16 @@
 class FooUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  version :small do
+  process :resize_to_fit => [500,500]
 
-    process :resize_to_fit => [100,100]
+  version :standard do
+    process :resize_to_fit => [250,250]
 
     version :mirror do
       process :flop
     end
   end
-  
+
   def flop
     manipulate! do |img|
       img.flop!
@@ -17,24 +18,3 @@ class FooUploader < CarrierWave::Uploader::Base
     end
   end
 end
-
-# class FooUploader < CarrierWave::Uploader::Base
-# 
-#   include CarrierWave::MiniMagick
-#   
-#   version :rotated do
-#     process :rotate
-# 
-#     version :boxed do
-#       process :resize_to_fit => [100, 100]
-#     end
-#   end
-# 
-#   def rotate
-#     manipulate! do |img|
-#       img.rotate 90
-#       img
-#     end
-#   end
-# 
-# end
